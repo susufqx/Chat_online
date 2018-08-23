750ab8da574d3a0aa9fa03eb7e140cf2f86558c8
rui.li <jiangsulirui@gmail.com>
2018-08-23 19:49:50 +0800

    [lirui] Clean the code

--- client/client.java
+++ client/client.java
@@ -3,123 +3,124 @@ package client;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class client implements ActionListener{
-	static	 client       		c 				= 		new client();
-	static	 boolean            t            	=	    false;
-	static 	 GrapheUser      	gu           	=   	new GrapheUser ();
-    static 	 String          	clientNom    	=    	null;
-    static   String				clientPwd	    =		null;
-    int             			connection  	=   	0;
-    /**
-     *  on considerer les clients et les mots de passe  sont :
-     *    client               mot de passe
-     *    davide                eisti0001
-     *    lucas                 eisti0002
-     *    bernard               eisti0003
-     *    paul                  eisti0004
-     *    李睿                   lirui
-     *    赵方圆					fangyuan
-     *    黄思哲					huangsizhe
-     *    郝老师					haozhaojun
-     **/   
-   //String   [] user    =   {"davide", "lucas", "bernard", "paul", "李睿", "赵方圆", "黄思喆", "郝老师"};
-   //String   [] pwd     =   {"eisti0001", "eisti0002", "eisti0003", "eisti0004", "lirui", "fangyuan", "huangsizhe", "haozhaojun"};
 
-   @Override
-   public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	String s  =  e.getActionCommand();
-	//int p = juger();
-	if (s.equals("Login")) {
-		clientNom	=  gu.user.getText();
-		clientPwd	=  gu.pwd.getText();
-		t = true;
-		//gu.frame1	.setVisible(false);
-		connection	=	1;
-		//new SocketClient(clientNom, clientPwd);
-		//c.auth();
-		//c.last();
-		if (gomain == 1) {
-			c.last();
-		}
-	}
-	if (s.equals("Annuler")) {
-		gu.user	.setText("");
-		gu.pwd	.setText("");
-	}
-}
-   /**
-    *  afficher les erreurs  
+public class client implements ActionListener{
+    static client c = new client();
+    static boolean t = false;
+    static GrapheUser gu = new GrapheUser ();
+    static String clientNom = null;
+    static String clientPwd = null;
+    int connection = 0;
+    /**
+    * on considerer les clients et les mots de passe  sont :
+    * client mot de passe
+    * davide eisti0001
+    * lucas eisti0002
+    * bernard eisti0003
+    * paul eisti0004
+    * 李睿 lirui
+    * 赵方圆 fangyuan
+    * 黄思哲 huangsizhe
+    * 郝老师 haozhaojun
+    **/
+    String [] user = {"davide", "lucas", "bernard", "paul", "李睿", "赵方圆", "黄思喆", "郝老师"};
+    String [] pwd = {"eisti0001", "eisti0002", "eisti0003", "eisti0004", "lirui", "fangyuan", "huangsizhe", "haozhaojun"};
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+        // int p = juger();
+        if (s.equals("Login")) {
+            clientNom = gu.user.getText();
+            clientPwd = gu.pwd.getText();
+            t = true;
+            // gu.frame1.setVisible(false);
+            connection = 1;
+            // new SocketClient(clientNom, clientPwd);
+            // c.auth();
+            // c.last();
+            if(gomain == 1) {
+                c.last();
+            }
+        }
+
+        if (s.equals("Annuler")) {
+            gu.user.setText("");
+            gu.pwd.setText("");
+        }
+    }
+    /**
+    *  afficher les erreurs
     */
-	public static void viewError (String error) {
-		JFrame   f    =    new JFrame ("Error");
-		JPanel   p    =    new JPanel ();
-		JLabel   l    =    new JLabel (error);
-		f . add  (p);
-		p . add	 (l);
-		f . setSize (250, 50);
-		f . setResizable(false);
-		f . setVisible (true);
-	}
-	
-	public void run () {
-	    gu  . login    .addActionListener(this);
-	    gu  . annuler  .addActionListener(this);
-		gu  . faireGui();
-	}
-	
-	public void last () {
-		//a.run();
-		while (true) {
-			while (SocketClient.a == 0) {
-				do {
-					System.out.println(t);
-				}while (t == false);
-				t = false; 
-				gomain = 1;
-				new SocketClient(clientNom, clientPwd);
-				SocketClient.a = 0;
-			}
-		}
-	}
-	int gomain = 0;
-	public static void main (String [] args) {
-		c.run();
-		//new SocketClient(clientNom, clientPwd);
-		c.last();
-	}
+    public static void viewError(String error) {
+        JFrame f = new JFrame("Error");
+        JPanel p = new JPanel();
+        JLabel l = new JLabel(error);
+        f.add(p);
+        p.add(l);
+        f.setSize(250, 50);
+        f.setResizable(false);
+        f.setVisible(true);
+    }
+
+    public void run() {
+        gu.login.addActionListener(this);
+        gu.annuler.addActionListener(this);
+        gu.faireGui();
+    }
+
+    public void last() {
+        //a.run();
+        while (true) {
+            while (SocketClient.a == 0) {
+                do {
+                    System.out.println(t);
+                } while (t == false);
+
+                t = false;
+                gomain = 1;
+                new SocketClient(clientNom, clientPwd);
+                SocketClient.a = 0;
+            }
+        }
+    }
+    int gomain = 0;
+    public static void main(String [] args) {
+        c.run();
+        //new SocketClient(clientNom, clientPwd);
+        c.last();
+    }
 }
 
 /**
- * pour affihcer le GUI de login 
+ * pour affihcer le GUI de login
  */
 class GrapheUser extends WindowAdapter{
-	// ******************** parametres de gui de login ************************
-    public  JFrame      frame1     =       new JFrame ("Login de Client");
-    public  JPanel      panel1     =       new JPanel ();
-    public  JLabel      userName   =       new JLabel ("     Nom de Client");
-    public  JTextField  user       =       new JTextField (25);
-    public  JLabel      pwdChar    =       new JLabel ("Votre mot de passe");
-    public  JTextField  pwd        =       new JTextField (25);
-    public  JButton     login      =       new JButton ("Login");
-    public  JButton     annuler    =       new JButton ("Annuler");
-    //public  static int   n  =  0;
-  
+    // ******************** parametres de gui de login ************************
+    public JFrame frame1 = new JFrame ("Login de Client");
+    public JPanel panel1 = new JPanel ();
+    public JLabel userName = new JLabel ("     Nom de Client");
+    public JTextField user = new JTextField (25);
+    public JLabel pwdChar = new JLabel ("Votre mot de passe");
+    public JTextField pwd = new JTextField (25);
+    public JButton login = new JButton ("Login");
+    public JButton annuler = new JButton ("Annuler");
+    //public static int n = 0;
+
     public void windowClosing(WindowEvent e) {
-       System.exit(0);
+        System.exit(0);
     }
-    public void faireGui () {
-        frame1   .add  (panel1);
-        panel1   .add  (userName);
-        panel1   .add  (user);
-        panel1   .add  (pwdChar);
-        panel1   .add  (pwd);
-        panel1   .add  (login);
-        panel1   .add  (annuler);
-        frame1   .addWindowListener(this);
-        
-        frame1   .setSize (500, 150);
-        frame1   .setResizable(false);  
-        frame1   .setVisible (true);
-    }    
+    public void faireGui() {
+        frame1.add(panel1);
+        panel1.add(userName);
+        panel1.add(user);
+        panel1.add(pwdChar);
+        panel1.add(pwd);
+        panel1.add(login);
+        panel1.add(annuler);
+        frame1.addWindowListener(this);
+        frame1.setSize(500, 150);
+        frame1.setResizable(false);
+        frame1.setVisible(true);
+    }
 }
--- cryptographie/RSA.java
+++ cryptographie/RSA.java
@@ -1,78 +1,83 @@
 package cryptographie;
 
-import java.security.KeyPair;  
-import java.security.KeyPairGenerator;  
-import java.security.interfaces.RSAPrivateKey;  
-import java.security.interfaces.RSAPublicKey;  
-import javax.crypto.Cipher;  
- 
-public class RSA  {    
-	// ********************** cle publique et privee **********************
-	private RSAPrivateKey privateKey;
-	private RSAPublicKey  publicKey;
-	/**
-	 * pour obtenir les cles qui ont produire par algo RSA de java
-	 * */
-    public void getKey () {
-        try   {   
-            KeyPairGenerator keyPairGen  =  KeyPairGenerator.getInstance("RSA");  
-            keyPairGen.  initialize(2048);    
-            KeyPair keyPair = keyPairGen.generateKeyPair();    
-            privateKey      =  (RSAPrivateKey)keyPair .getPrivate();    // produire le cle de privee
-            publicKey       =  (RSAPublicKey)keyPair  .getPublic();     // produire le cle de publique         
-        }   
-        catch (Exception e)   {  
-            e.printStackTrace();   
-        }  
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import javax.crypto.Cipher;
+
+
+/**
+* RSA PART
+*/
+public class RSA  {
+    // ********************** cle publique et privee **********************
+    private RSAPrivateKey privateKey;
+    private RSAPublicKey publicKey;
+    /**
+    * pour obtenir les cles qui ont produire par algo RSA de java
+    **/
+
+    public void getKey() {
+        try {
+            KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+            keyPairGen.initialize(2048);
+            KeyPair keyPair = keyPairGen.generateKeyPair();
+            privateKey = (RSAPrivateKey)keyPair.getPrivate();    // produire le cle de privee
+            publicKey = (RSAPublicKey)keyPair.getPublic();     // produire le cle de publique
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     /**
      *  retour le cle publique
      * */
-    public RSAPublicKey getPubKey () {
-    	return publicKey;
+    public RSAPublicKey getPubKey() {
+        return publicKey;
     }
     /**
      *  retour le cle privee
      * */
-    public RSAPrivateKey getPriKey () {
-    	return privateKey;
+    public RSAPrivateKey getPriKey() {
+        return privateKey;
     }
     /**
      * crypter par RSA
      * */
-    public byte[] encrypt(RSAPublicKey publicKey, byte[] srcBytes)  {   
-        if (publicKey != null)   {   
-            try  {   
-                Cipher cipher = Cipher.getInstance("RSA");   
-                // pour initialisation 
-                cipher.init(Cipher.ENCRYPT_MODE, publicKey);   
+    public byte[] encrypt(RSAPublicKey publicKey, byte[] srcBytes) {
+        if (publicKey != null) {
+            try {
+                Cipher cipher = Cipher.getInstance("RSA");
+                // pour initialisation
+                cipher.init(Cipher.ENCRYPT_MODE, publicKey);
                 // crypter le messages par RSA
-                byte[] resultBytes  = cipher.doFinal(srcBytes);  
-                return resultBytes;  
-            }   
-            catch (Exception e)  {   
-                e.printStackTrace();   
-            }   
-        }   
-        return null;   
-    }  
+                byte[] resultBytes = cipher.doFinal(srcBytes);
+                return resultBytes;
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
     /**
      * decrypter par RSA
      * */
-    public byte[] decrypt(RSAPrivateKey privateKey, byte[] encBytes)   {   
-        if (privateKey != null)   {   
-            try  {   
-                Cipher cipher = Cipher.getInstance("RSA");  
-                // pour initialisation 
-                cipher.init(Cipher.DECRYPT_MODE, privateKey);  
+    public byte[] decrypt(RSAPrivateKey privateKey, byte[] encBytes)   {
+        if (privateKey != null) {
+            try {
+                Cipher cipher = Cipher.getInstance("RSA");
+                // pour initialisation
+                cipher.init(Cipher.DECRYPT_MODE, privateKey);
                 // crypter le messages par RSA
-                byte[] decBytes  = cipher.doFinal(encBytes);  
-                return decBytes;  
-            }   
-            catch (Exception e)   {   
-                e.printStackTrace();   
-            }   
-        }   
-        return null;   
-    }   
-}  
+                byte[] decBytes = cipher.doFinal(encBytes);
+                return decBytes;
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
